2f0ea1bdde4aeae121f1dacc100b14d6e87a830c
SnaidenP <fredd85565@gmail.com>
2024-03-22 18:39:21 -0400

    setup project

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+void main() =>
+    runApp(DevicePreview(enabled: true, builder: (context) => const MyApp()));
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
